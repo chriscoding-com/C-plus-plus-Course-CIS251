88a5d8a0329fef903ff29d61dee639cd624d0165
Chris Coding <chris.s@shogamer.com>
2020-01-15 21:39:44 -0600

    Commit Try

---
+++ Assignment1/Assignment1-2/Assignment1-2/Assignment1-2.cpp
@@ -0,0 +1,26 @@
+// Assignment1-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*************************************************\n";
+    cout << "                                                 \n";
+    cout << "       C C C             s s s s        II       \n";
+    cout << "     C       C         S         S      ii       \n";
+    cout << "    C                 S                 ii       \n";
+    cout << "   C                   S                II       \n";
+    cout << "   C                    S S S S         II       \n";
+    cout << "   C                             S      II       \n";
+    cout << "    C                             S     II       \n";
+    cout << "     C       C         S         $               \n";
+    cout << "        C C C            S S S S        00       \n";
+    cout << "                                                 \n";
+    cout << "*************************************************\n";
+    cout << "                                                 \n";
+    cout << "Computer Science is  Cool Stuff!!!               \n";
+    cout << "                                                 \n";
+    cout << "*************************************************\n";
+}
